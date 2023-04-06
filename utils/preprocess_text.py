4f6c41424ae912b75b30e546f6f3423a50abe589
Raoufdine SAID <said.raoufdine@gmail.com>
2023-04-06 13:27:03 +0200

    Notebook from raw dataset to labelized dataset ready

--- utils/preprocess_text.py
+++ utils/preprocess_text.py
@@ -12,15 +12,17 @@ import nltk
 
 # Preprocess a text
 def preprocess_text(text, glovemgr, is_sep_n = False, remove_stop_word = True, stemming=True, trunc_sent=-1, padding_sent=-1):
-    # lower
-    result = text.lower()
-
+    result = text
+    
     # tokenize sentence
     if is_sep_n:
         nltk_line_tokenizer = LineTokenizer()
         result = nltk_line_tokenizer.tokenize(result)
     else:
         result = sent_tokenize(result)
+
+    # lower
+    result = [line.lower() for line in result]
 
     # Remove punctuation
     result = ["".join([char for char in line if char not in string.punctuation]) for line in result]
